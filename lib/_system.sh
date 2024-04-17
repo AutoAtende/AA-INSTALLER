d3683d5d56e1cee1f0b1e1b138e5ebb5f39e21fe
Lucas Saud <lucas@autoatende.com>
2024-04-17 01:14:25 -0300

    Update _system.sh
    - removido as dependencias do puppetter
    - versao do node volltou para a 16.20

--- lib/_system.sh
+++ lib/_system.sh
@@ -281,7 +281,7 @@ system_node_install() {
   sleep 2
 
   sudo su - root <<EOF
-  curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
   apt-get install -y nodejs
   sleep 2
   npm install -g npm@latest
@@ -398,54 +398,6 @@ system_puppeteer_dependencies() {
   print_banner
   printf "${WHITE} 💻 Instalando puppeteer dependencies...${GRAY_LIGHT}"
   printf "\n\n"
-
-  sleep 2
-
-  sudo su - root <<EOF
-  apt-get install -y libxshmfence-dev \
-                      libgbm-dev \
-                      wget \
-                      unzip \
-                      fontconfig \
-                      locales \
-                      gconf-service \
-                      libasound2 \
-                      libatk1.0-0 \
-                      libc6 \
-                      libcairo2 \
-                      libcups2 \
-                      libdbus-1-3 \
-                      libexpat1 \
-                      libfontconfig1 \
-                      libgcc1 \
-                      libgconf-2-4 \
-                      libgdk-pixbuf2.0-0 \
-                      libglib2.0-0 \
-                      libgtk-3-0 \
-                      libnspr4 \
-                      libpango-1.0-0 \
-                      libpangocairo-1.0-0 \
-                      libstdc++6 \
-                      libx11-6 \
-                      libx11-xcb1 \
-                      libxcb1 \
-                      libxcomposite1 \
-                      libxcursor1 \
-                      libxdamage1 \
-                      libxext6 \
-                      libxfixes3 \
-                      libxi6 \
-                      libxrandr2 \
-                      libxrender1 \
-                      libxss1 \
-                      libxtst6 \
-                      ca-certificates \
-                      fonts-liberation \
-                      libappindicator1 \
-                      libnss3 \
-                      lsb-release \
-                      xdg-utils
-EOF
 
   sleep 2
 }
